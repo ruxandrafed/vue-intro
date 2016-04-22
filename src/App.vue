cc5b37daf735094ecd6ebc02f4c67a52ca1cf611
Bo Xing <xingbo828@gmail.com>
2016-04-21 23:11:35 -0700

    step1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
